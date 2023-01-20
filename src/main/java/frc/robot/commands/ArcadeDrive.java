581985a15f7bd51838b9a4771855ef8dfb0c7f92
JMHS Robotics Laptop 2 <jmhsrobotics@johnmarshallit.com>
2023-01-20 16:23:06 -0500

    slow mode works with a default value, need to get it working with a button press

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 //Imports
 import java.util.function.Supplier;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.Drivetrain;
 //End Imports
 
@@ -10,26 +11,44 @@ public class ArcadeDrive extends CommandBase {
 
     private final Drivetrain drivetrain;
     private final Supplier<Double> forwardSpeed, turnSpeed;
+    public static boolean isSlow = false;
 
     public ArcadeDrive(Drivetrain drivetrain, Supplier<Double> forwardSpeed, Supplier<Double> turnSpeed) {
         this.drivetrain = drivetrain;
         this.forwardSpeed = forwardSpeed;
         this.turnSpeed = turnSpeed;
         addRequirements(drivetrain);
+       
     }
 
     // Runs when ArcadeDrive is initialized
     @Override
     public void initialize() {
+        System.out.println("init ArcadeDrive");
+        isSlow = true;
     }
 
     // Called
     @Override
     public void execute() {
         System.out.println(forwardSpeed.get() + " forward " + turnSpeed.get() + " turn ");
+  
+
+        if(isSlow){
+            this.drivetrain.m_robotDrive.arcadeDrive(forwardSpeed.get() * DriveConstants.SLOW_SPEED_FRACTION, turnSpeed.get() * DriveConstants.SLOW_SPEED_FRACTION);
+        }
+        else{
+            this.drivetrain.m_robotDrive.arcadeDrive(forwardSpeed.get(), turnSpeed.get());
+        }
+
+    }
+
+    public void slowMode() {
+        System.out.println(forwardSpeed.get() + " forward " + turnSpeed.get() + " turn ");
         this.drivetrain.m_robotDrive.arcadeDrive(forwardSpeed.get() / 10.0, turnSpeed.get() / 10.0);
 
     }
+
 
     @Override
     public void end(boolean interrupted) {
