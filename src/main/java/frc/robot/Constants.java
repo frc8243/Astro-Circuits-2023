fbe460fef24ce41017be59c4c58f3997d2b45c5b
JMHS Robotics Laptop 1 <jmhsrobotics@johnmarshallit.com>
2023-02-28 17:09:57 -0500

    Finished PID tuning!!!

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -52,9 +52,9 @@ public class Constants {
     }
 
     public static final class ArmConstants{
-        public static final double kP = 0.5;
+        public static final double kP = 10;
         public static final double kI = 0.0;
-        public static final double kD = 0.0;
+        public static final double kD = 0.1;
         public static final int kArmMotor = 14;
         public static final double kGVolts = 0.133;
         public static final double kSVolts = 2;
