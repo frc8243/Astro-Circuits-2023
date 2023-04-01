3c9911394355585c7c1effd82d44343b712ad77f
JMHS Robotics Laptop 1 <jmhsrobotics@johnmarshallit.com>
2023-04-01 09:45:33 -0400

    SPEED OF AUTON!

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -77,7 +77,7 @@ public class Constants {
         public static final double kArmMaxAngle = Units.degreesToRadians(180);
         public static final double kArmOffsetInDegrees = Units.degreesToRadians(60);
         public static final double kArmLoadingLocation = Units.degreesToRadians(107);
-        public static final double kArmRestingLocation = Units.degreesToRadians(5);
+        public static final double kArmRestingLocation = Units.degreesToRadians(15);
         public static final double kArmScoringLocation = Units.degreesToRadians(105);
         public static final double kArmCubeScoringLocation = Units.degreesToRadians(110);
         public static final double kArmToFloor = Units.degreesToRadians(190);
--- src/main/java/frc/robot/commands/drivetrain/DriveForwardGivenDistanceUsingTime.java
+++ src/main/java/frc/robot/commands/drivetrain/DriveForwardGivenDistanceUsingTime.java
@@ -11,7 +11,7 @@ import frc.robot.subsystems.Drivetrain;
 public class DriveForwardGivenDistanceUsingTime extends CommandBase {
 
   Timer timer;
-  static double SPEED = 1/3.;
+  static double SPEED = 1/2.;
   double time;
   Drivetrain m_driveTrain;
   double distance;
@@ -20,7 +20,7 @@ public class DriveForwardGivenDistanceUsingTime extends CommandBase {
       // initial variables
       m_driveTrain = subsystem;
       addRequirements(m_driveTrain);
-      time = Math.abs(distance * .85);
+      time = Math.abs(distance * .5666666666666667); /* This number is 1/V */ //TODO : Make this t = d/v
       this.distance = distance;
       // set command to be interuptible
       // setInterruptible(true);
